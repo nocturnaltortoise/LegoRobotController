5881cba07a5638a8f4ee158eabee6ff2f2bdfac3
Alex Adams <aadams3@sheffield.ac.uk>
2014-12-04 12:59:58 +0000

    Added core imports and connection

--- Robot.java
+++ Robot.java
@@ -1,3 +1,15 @@
 /**
 * @author Alex Adams, Simon Turner
 */
+
+import icommand.nxt.*;
+import icommand.nxt.comm.NXTCommand;
+
+public class Robot {
+	public static void main(String[] args) {
+
+		NXTCommand.open();
+		NXTCommand.setVerify(true);
+
+	}
+}
