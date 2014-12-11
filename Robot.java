09ea1f0c2c6bad55e4ade8ded2f7464a87fe31be
Alex Adams <aadams3@sheffield.ac.uk>
2014-12-11 13:58:26 +0000

    Added conditions

--- Robot.java
+++ Robot.java
@@ -8,18 +8,38 @@ import icommand.nxt.comm.NXTCommand;
 public class Robot {
 	public static void main(String[] args) throws InterruptedException {
 		final int INTERVAL = 100;
+		final int CUTOFF = 60;
+		final int DISTANCE = 20;
+		int lightLevel = 0;
+		boolean goalReached = false;
 
 		NXTCommand.open();
 		NXTCommand.setVerify(true);
 
-		LightSensor l = new LightSensor(SensorPort.S3);
+		LightSensor floorSensor = new LightSensor(SensorPort.S3);
+		UltrasonicSensor distSensor = new UltrasonicSensor(SensorPort.S2)
 
-		while (true) {
-			System.out.println(l.getLightPercent());
+		while (!goalReached) {
+			lightLevel = floorSensor.getLightPercent());
+			System.out.println(lightLevel);
+			if (distSensor.getDistance() < DISTANCE) {
+				System.out.println("Object infront");
+			}
+			if (lightLevel < CUTOFF) {
+				System.out.println("Not on black line");
+			}
 			Thread.sleep(INTERVAL);
 		}
 
-		NXTCommand.close();
+		dance()
+
+	}
+
+	public void turnLeft() {
+		// use of motors
+	}
+
+	public void dance() {
 
 	}
 }
