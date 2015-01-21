3910b9e70a7bb1c06f2f3cb3f70595918785baf6
Simon Turner <STurner4@sheffield.ac.uk>
2015-01-21 14:00:44 +0000

    added separate light levels for different sensors

--- Robot.java
+++ Robot.java
@@ -7,44 +7,51 @@ import icommand.nxt.comm.NXTCommand;
 
 public class Robot {
 
-	final static int LINECUTOFF = 48;
-	final static int SPOTCUTOFF = 40;
+	final static int LEFTLINECUTOFF = 600;
+	final static int RIGHTLINECUTOFF = 510;
+	final static int SPOTCUTOFF = 0;
 	final static int DISTANCE = 20;
+	static LightSensor leftSensor = new LightSensor(SensorPort.S1);
+	static LightSensor rightSensor = new LightSensor(SensorPort.S2);
 
 	public static void main(String[] args) throws InterruptedException {
-		final int INTERVAL = 100;
+		final int INTERVAL = 10;
 
 		NXTCommand.open();
 		NXTCommand.setVerify(true);
-
-		LightSensor leftSensor = new LightSensor(SensorPort.S1);
-		LightSensor rightSensor = new LightSensor(SensorPort.S2);
+		
 		UltrasonicSensor distSensor = new UltrasonicSensor(SensorPort.S4);
 
 		// 33 - Black Spot
 		// 50 - Black Line
 		// 60 - Floor
 
-		// Motor A - Right Wheel
+		// Motor C - Right Wheel
 		// Motor B - Left Wheel
 
 
-		while(!lineDetected(getLight(leftSensor)) && !lineDetected(getLight(rightSensor))) {
+		while(!lineDetected(leftSensor) && !lineDetected(rightSensor)) {
+			System.out.println("Left: " + getLight(leftSensor));
+			System.out.println("Right: " + getLight(rightSensor));
 			forward();
 		}		
 
-		turnLeft();
+		hardTurnLeft();
 
 		while (true) {
 
-			if (lineDetected(getLight(leftSensor)) && !lineDetected(getLight(rightSensor))) { // Turn Left
+			System.out.println("Left: " + getLight(leftSensor));
+			System.out.println("Right: " + getLight(rightSensor));
+
+			if (lineDetected(leftSensor) && !lineDetected(rightSensor)) { // Turn Left
 				turnLeft();
-			} else if (!lineDetected(getLight(leftSensor)) && lineDetected(getLight(rightSensor))) { // Turn Right
+			} else if (!lineDetected(leftSensor) && lineDetected(rightSensor)) { // Turn Right
 				turnRight();
-			} else if (obstacleDetected(distSensor) && lineDetected(getLight(leftSensor)) && lineDetected(getLight(rightSensor))) { // Turn at obstacle and both sensor on line
-				turnRight();
-			} else if (lineDetected(getLight(leftSensor)) && lineDetected(getLight(rightSensor))) { // Found Spot
+			} else if (obstacleDetected(distSensor) && (lineDetected(leftSensor) || lineDetected(rightSensor))) { // Turn at obstacle and both sensor on line
+				hardTurnRight();
+			} else if (lineDetected(leftSensor) && lineDetected(rightSensor) && !obstacleDetected(distSensor)) { // Found Spot
 				System.out.println("Spot");
+				stop();
 				//dance();
 			} else { // On line, moving forward
 				forward();
@@ -56,8 +63,8 @@ public class Robot {
 
 
 		// while(true){
-		// 	System.out.println("Left Sensor: " + leftSensor.getLightPercent());
-		// 	System.out.println("Right Sensor: " + rightSensor.getLightPercent());
+		// 	System.out.println("Left Sensor: " + leftSensor.getLightValue());
+		// 	System.out.println("Right Sensor: " + rightSensor.getLightValue());
 
 		// 	Thread.sleep(INTERVAL);
 		// }
@@ -67,55 +74,84 @@ public class Robot {
 
 	private static int getLight(LightSensor sensor) {
 
-		return sensor.getLightPercent();
+		return sensor.getLightValue();
 
 	}
 
-	private static boolean lineDetected(int lightPercent){
+	private static boolean lineDetected(LightSensor sensor){
 
-		if(lightPercent < LINECUTOFF){
-			blackDetectCount++;
-			return true;
+		int lightValue = getLight(sensor);
+
+		if (sensor.equals(leftSensor)) {
+			if (lightValue < LEFTLINECUTOFF) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			if (lightValue < RIGHTLINECUTOFF) {
+				return true;
+			} else {
+				return false;
+			}
 		}
+
+
 		
 	}
 
 	private static boolean obstacleDetected(UltrasonicSensor sensor) {
 
 		if (sensor.getDistance() < DISTANCE) {
-			return true
+			return true;
+		}else{
+			return false;
 		}
 
 	}
 
 	private static void forward() {
 		System.out.println("Forward");
-		Motor.A.setSpeed(100);
 		Motor.B.setSpeed(100);
-		Motor.A.forward();
+		Motor.C.setSpeed(100);
 		Motor.B.forward();
+		Motor.C.forward();
 	}
 
 	private static void turnLeft() {
 		System.out.println("Turning Left");
-		Motor.A.setSpeed(50);
-		Motor.B.setSpeed(50);
-		Motor.A.forward();
+		Motor.C.setSpeed(40);
+		Motor.B.setSpeed(40);
+		Motor.C.forward();
 		Motor.B.backward();
 	}
 
 	private static void turnRight() {
 		System.out.println("Turning Right");
-		Motor.A.setSpeed(50);
-		Motor.B.setSpeed(50);
-		Motor.A.backward();
+		Motor.C.setSpeed(40);
+		Motor.B.setSpeed(40);
+		Motor.C.backward();
 		Motor.B.forward();
-
 	}
 
+	private static void  hardTurnLeft() {
+		System.out.println("Hard Turning Left");
+		Motor.C.setSpeed(200);
+		Motor.B.setSpeed(200);
+		Motor.C.forward();
+		Motor.B.backward();
+	}
+
+	private static void hardTurnRight() {
+		System.out.println("Hard Turning Right");
+		Motor.C.setSpeed(200);
+		Motor.B.setSpeed(200);
+		Motor.C.backward();
+		Motor.B.forward();
+	}
 	private static void stop() {
 		System.out.println("Stopped");
-		Motor.A.stop();
+		Motor.C.stop();
 		Motor.B.stop();
 	}
 
@@ -126,9 +162,9 @@ public class Robot {
 
 	private static void spin() {
 		System.out.println("Spinning");
-		Motor.A.setSpeed(150);
+		Motor.C.setSpeed(150);
 		Motor.B.setSpeed(150);
-		Motor.A.forward();
+		Motor.C.forward();
 		Motor.B.backward();
 	}
 
